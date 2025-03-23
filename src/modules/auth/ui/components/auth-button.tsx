ae7ad75a2c95bff0e6a804e23cd2e7b1a23dc6f8
snorlax.karan@gmail.com <snorlax.karan@gmail.com>
2025-03-23 07:49:28 +0530

    adding studio

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { UserCircleIcon } from "lucide-react";
+import { ClapperboardIcon, UserCircleIcon } from "lucide-react";
 
 import React from "react";
 
@@ -10,7 +10,15 @@ export const AuthButton = () => {
   return (
     <>
       <SignedIn>
-        <UserButton />
+        <UserButton>
+          <UserButton.MenuItems>
+            <UserButton.Link
+              label="Studio"
+              href="/studio"
+              labelIcon={<ClapperboardIcon className="size-4" />}
+            />
+          </UserButton.MenuItems>
+        </UserButton>
         {/* Add menu items for studio and user profile */}
       </SignedIn>
       <SignedOut>
